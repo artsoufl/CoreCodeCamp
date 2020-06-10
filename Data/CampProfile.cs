378a362ec1d3a257129a0ae3870435a57630657f
artsoufl <artsoufl@gmail.com>
2020-06-10 20:19:41 +0100

    create association api

--- Data/CampProfile.cs
+++ Data/CampProfile.cs
@@ -10,13 +10,20 @@ namespace CoreCodeCamp.Data
     {
         public CampProfile()
         {
-            this.CreateMap<Camp, CampModel>(); //.MaxDepth(3).PreserveReferences();
+            this.CreateMap<Camp, CampModel>().ReverseMap(); //.MaxDepth(3).PreserveReferences();
             //.ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
-            this.CreateMap<Talk, TalkModel>();
+            //this.CreateMap<Talk, TalkModel>();
 
             this.CreateMap<CampModel, Camp>()
                 .ForPath(c => c.Location.VenueName, o => o.MapFrom(m =>m.LocationVenueName));
-            this.CreateMap<TalkModel, Talk>();
+            //this.CreateMap<TalkModel, Talk>();
+            this.CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                .ForMember(t => t.Camp, opt => opt.Ignore()) // ignoring the mapping
+                .ForMember(t => t.Speaker, opt => opt.Ignore());
+
+            this.CreateMap<Speaker, SpeakerModel>().ReverseMap();
+            //this.CreateMap<Speaker, SpeakerModel>();
         }
     }
 }
--- Models/TalkModel.cs
+++ Models/TalkModel.cs
@@ -1,6 +1,7 @@
 ﻿using CoreCodeCamp.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CoreCodeCamp.Models
@@ -8,10 +9,14 @@ namespace CoreCodeCamp.Models
     public class TalkModel
     {
         public int TalkId { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
+        [Required]
         public string Abstract { get; set; }
+        [Range(100,500)]
         public int Level { get; set; }
 
-        //public SpeakerModel Speaker { get; set; }
+        public SpeakerModel Speaker { get; set; }
     }
 }
